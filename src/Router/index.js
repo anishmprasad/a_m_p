13af3b51cd6de2a851251b418674666e3ba30614
anish.m.prasad <anish.m.prasad@gmail.com>
2018-11-16 13:51:21 +0530

    folder update

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
+// import 'styleCommon/styles.scss';
+// import DynamicRoute from 'components/DynamicRoute';
+// import HelloWorld from 'components/HelloWorld'
+// import Graph from '../Graph';
+import Home from '../components/Home'
+import Header from '../components/Header'
+
 
 export default function Router(props) {
-  return(
-    <div className="router">
-    </div>
+  return (
+    <Fragment>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </Fragment>
   )
 }
+
