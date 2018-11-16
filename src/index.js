1492858cc25e5cf83dc92391a4101fddbf841554
anish.m.prasad <anish.m.prasad@gmail.com>
2018-11-16 05:51:10 +0530

    structure update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
-import Root from 'containers/Root';
-import configureStore from './store/configureStore';
-import 'babel-polyfill';
-const store = configureStore();
-const rootElement = document.getElementById('root');
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-render( <Root store={store} />, rootElement );
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
