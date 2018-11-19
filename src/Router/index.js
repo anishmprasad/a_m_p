faa3310fd93e189a7ed3db44e07fb617d766a93c
anish.m.prasad <anish.m.prasad@gmail.com>
2018-11-19 12:06:14 +0530

    update

--- src/Router/index.js
+++ src/Router/index.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 // import Home from '../components/Home'
 // import Header from '../components/Header'
 // import About from '../components/About';
-import './index.scss'
+import './index.scss';
 
 const OpenSource = lazy(() => import('../components/OpenSource'));
 const Home = lazy(() => import('../components/Home'));
@@ -12,21 +12,21 @@ const About = lazy(() => import('../components/About'));
 const Header = lazy(() => import('../components/Header'));
 
 
-export default function Router(props) {
+export default function Router() {
   return (
     <div className="wrapper">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div className="header">Loading...</div>}>
         <Header />
       </Suspense>
       <Switch>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Route exact path='/' component={Home} />
+        <Suspense fallback={<div className="home">Loading...</div>}>
+          <Route exact path='/' render={() => <Home/>} />
         </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Route exact path='/opensource' component={OpenSource} />
+        <Suspense fallback={<div className="opensource">Loading...</div>}>
+          <Route exact path='/opensource' render={() => <OpenSource/>} />
         </Suspense>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Route exact path='/about' component={About} />
+        <Suspense fallback={<div className="about">Loading...</div>}>
+          <Route exact path='/about' render={() => <About/>} />
         </Suspense>
       </Switch>
     </div>
