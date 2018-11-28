15364c17b154fe479253b833b70e8ef4189f45d4
anish.m.prasad <anish.m.prasad@gmail.com>
2018-11-28 11:39:46 +0530

    update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,101 +1,46 @@
 import React, { Component } from 'react';
 import './index.scss';
 
-
-// function consoleText(words, animatedText,consoleText, colors) {
-//   console.log(animatedText)
-//   if (colors === undefined) colors = ['#fff'];
-//   var visible = true;
-//   var con = consoleText;
-//   var letterCount = 1;
-//   var x = 1;
-//   var waiting = false;
-//   var target = animatedText;
-//   target.setAttribute('style', 'color:' + colors[0])
-//   window.setInterval(function() {
-
-//     if (letterCount === 0 && waiting === false) {
-//       waiting = true;
-//       target.innerHTML = words[0].substring(0, letterCount)
-//       window.setTimeout(function() {
-//         var usedColor = colors.shift();
-//         colors.push(usedColor);
-//         var usedWord = words.shift();
-//         words.push(usedWord);
-//         x = 1;
-//         target.setAttribute('style', 'color:' + colors[0])
-//         letterCount += x;
-//         waiting = false;
-//       }, 1000)
-//     } else if (letterCount === words[0].length + 1 && waiting === false) {
-//       waiting = true;
-//       window.setTimeout(function() {
-//         x = -1;
-//         letterCount += x;
-//         waiting = false;
-//       }, 1000)
-//     } else if (waiting === false) {
-//       target.innerHTML = words[0].substring(0, letterCount)
-//       letterCount += x;
-//     }
-//   }, 120)
-//   window.setInterval(function() {
-//     if (visible === true) {
-//       con.className = 'console-underscore hidden'
-//       visible = false;
-
-//     } else {
-//       con.className = 'console-underscore'
-
-//       visible = true;
-//     }
-//   }, 400)
-// }
-
 export default class Home extends Component{
   componentDidMount(){
-    // consoleText(
-    //   [
-    //     'I"m Anish',
-    //     'I like web development',
-    //     'react',
-    //     'react-native',
-    //     'redux',
-    //     'jest',
-    //     'JavaScript',
-    //     'SCSS',
-    //     'PHP'
-    //   ],
-    //   this.animatedText,
-    //   this.consoleText,
-    //   ['tomato', 'rebeccapurple', 'lightblue']
-    // );
   }
   render(){
     return(
       <div className="home">
-        content
-        {/* <span 
-          ref={(ref) => this.animatedText = ref}
-          className="text"
-        />
-        <div
-          ref={(ref) => this.consoleText = ref} 
-          className='console-underscore' 
-          id='console'>&#95;
-        </div> */}
-
-
-        {/* <div className="content">
-          <div className="static">
-              <p>I am a</p>
-              <ul>
-                  <li>Designer</li>
-                  <li>Developer</li>
-                  <li>Magician</li>
-              </ul>
+        {/* <section class="container">
+          <h1 class="title">
+            <span>Hi, nice</span>
+            <span>to see</span>
+            <span>you here</span>
+          </h1>
+          
+          <h2 class="title">
+            <span>I'm</span>
+            <span>Anish M Prasad</span>
+          </h2>
+          <h3 class="title">
+            <span>I Love </span>
+            <span>to create</span>
+            <span>things</span>
+          </h3>
+        </section> */}
+        <div className = "section definition">
+          <div className = "grid-full-width" >
+            <dl>
+              <dt>
+                <span className = "word">Co</span>
+                <span className = "bullet"> </span>
+                <span className = "word slide-this" > here </span>
+                <span className = "pronunciation" > /ko’hir/
+                  <strong>Verb</strong>
+                </span>
+              </dt>
+              <dd><sup>1.</sup> to be united; form a whole</dd>
+              <dd><sup>2.</sup> to be aesthetically and logically consistent</dd>
+              <dd><sup>3.</sup> to become united in principles, relationships or interests</dd>
+            </dl>
           </div>
-        </div> */}
+        </div>
       </div>
     )
   }
