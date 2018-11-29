752aaa5691073c3cf43d4ac62988f011d2bfbb4e
anish.m.prasad <anish.m.prasad@gmail.com>
2018-11-29 23:44:22 +0530

    saga update

--- src/sagas/Projects/Graph/index.js
+++ src/sagas/Projects/Graph/index.js
@@ -8,14 +8,14 @@ import { all, put, select, takeLatest, take, call, fork } from 'redux-saga/effec
 import { CanvasData, isProjectChanged } from '../../../actions/Projects/Graph';
 import { CANVAS_DATA, IS_PROJECT_CHANGED } from '../../../actionTypes/Projects/Graph';
 
-export function* CanvasData(node) {
+export function* CanvasSaga(node) {
 	yield put(CanvasData(node));
 }
 
-export function* isProjectChanged(object) {
+export function* isProjectChangedSaga(object) {
 	yield put(isProjectChanged(object));
 }
 // Saga function that is initiated in the beginning to be able to listen to CANVAS_DATA and IS_PROJECT_CHANGED  action
 export default function* root() {
-	yield all([takeLatest(CANVAS_DATA, CanvasData)], [takeLatest(IS_PROJECT_CHANGED, isProjectChanged)]);
+	yield all([takeLatest(CANVAS_DATA, CanvasSaga)], [takeLatest(IS_PROJECT_CHANGED, isProjectChangedSaga)]);
 }
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,10 +3,11 @@ import { all, fork } from 'redux-saga/effects';
 // import app from './App';
 // import github from './github';
 import OpenSource from './OpenSource';
+import Graph from './Projects/Graph';
 
 /**
  * rootSaga
  */
 export default function* root() {
-	yield all([fork(OpenSource)]);
+	yield all([fork(OpenSource)], [fork(Graph)]);
 }
