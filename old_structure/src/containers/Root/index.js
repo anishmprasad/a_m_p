6440f3347f01fa67bf9d15536517bfbaba102757
anish.m.prasad <anish.m.prasad@gmail.com>
2018-12-06 13:48:04 +0530

    flow update

--- old_structure/src/containers/Root/index.js
+++ old_structure/src/containers/Root/index.js
@@ -1,30 +1,27 @@
-import React, { Component } from "react";
-import { Provider } from "react-redux";
-import PropTypes from "prop-types";
-import Router from "../../components/Router";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-class Root extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      onEnter: false
-    };
-  }
-  render() {
-      return (
-        <Provider store={this.props.store}>
-          {
-            <BrowserRouter>
-              <Route path="/" component={Router} />
-            </BrowserRouter>
-          }
-        </Provider>
-      );
-  }
-}
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import Router from '../../components/Router';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-Root.propTypes = {
-  store: PropTypes.object
+type props = {
+	store: Object
 };
-
-export default Root;
+export default class Root extends Component<props> {
+	constructor(props) {
+		super(props);
+		this.state = {
+			onEnter: false
+		};
+	}
+	render() {
+		return (
+			<Provider store={this.props.store}>
+				{
+					<BrowserRouter>
+						<Route path='/' component={Router} />
+					</BrowserRouter>
+				}
+			</Provider>
+		);
+	}
+}
--- src/Router/index.js
+++ src/Router/index.js
@@ -5,30 +5,30 @@ import Loader from '../components/Loader';
 import './index.scss';
 import AnimatedTransition from '../components/AnimatedTransition';
 
-const OpenSource = lazy(() => import('../containers/OpenSource'));
-const Home = lazy(() => import('../components/Home'));
-const About = lazy(() => import('../components/About'));
-const Header = lazy(() => import('../components/Header'));
-const Container = lazy(() => import('../components/Container'))
-const OpenSourceProject = lazy(() => import('../containers/OpenSourceProject'));
-
+const OpenSource = lazy(() => import(/* webpackChunkName: "OpenSource" */ '../containers/OpenSource'));
+const Home = lazy(() => import(/* webpackChunkName: "Home" */ '../components/Home'));
+const About = lazy(() => import(/* webpackChunkName: "About" */ '../components/About'));
+const Header = lazy(() => import(/* webpackChunkName: "Header" */ '../components/Header'));
+const Container = lazy(() => import(/* webpackChunkName: "Container" */ '../components/Container'));
+const OpenSourceProject = lazy(() =>
+	import(/* webpackChunkName: "OpenSourceProject" */ '../containers/OpenSourceProject')
+);
 
 export default function Router() {
-  return (
-    <div className='wrapper'>
-      <Suspense fallback={<Loader/>}>
-        <Header />
-        <Container>
-          <Switch>
-          <Route exact path='/' render={() => <Home />} />
-          <Route exact path='/opensource' render={() => <OpenSource />} />
-          <Route exact path='/opensource/:project' render={() => <OpenSourceProject />} />
-          <Route exact path='/about' render={() => <About />} />
-          <Route exact path='/animatedtransition' render={() => <AnimatedTransition />} />
-        </Switch>
-        </Container>
-      </Suspense>
-    </div>
-  )
+	return (
+		<div className='wrapper'>
+			<Suspense fallback={<Loader />}>
+				<Header />
+				<Container>
+					<Switch>
+						<Route exact path='/' render={() => <Home />} />
+						<Route exact path='/opensource' render={() => <OpenSource />} />
+						<Route exact path='/opensource/:project' render={() => <OpenSourceProject />} />
+						<Route exact path='/about' render={() => <About />} />
+						<Route exact path='/animatedtransition' render={() => <AnimatedTransition />} />
+					</Switch>
+				</Container>
+			</Suspense>
+		</div>
+	);
 }
-
