12843d470b99b2d35abe9a85818c0c1350190cc5
anish.m.prasad <anish.m.prasad@gmail.com>
2018-12-17 22:26:07 +0530

    app update

--- src/App/index.js
+++ src/App/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
 	render() {
 		return (
 			<div className='App'>
@@ -19,5 +19,3 @@ class App extends Component {
 		);
 	}
 }
-
-export default App;
