1f1d57ac29804ec5e9e7064b93a645d47a2f61d9
Anish M Prasad <Anish.m.prasad@gmail.com>
2019-01-26 23:03:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
-// import App from './App';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './store/configureStore';
 import Root from './Root';
