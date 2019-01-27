a18bbeb962b7d37cd4f82b4bb1edee37e46d799a
Anish M Prasad <Anish.m.prasad@gmail.com>
2019-01-27 23:07:24 +0530

    Update index.js

--- src/actions/OpenSource/index.js
+++ src/actions/OpenSource/index.js
@@ -1,7 +1,5 @@
-// import axios from 'axios';
 // import { CANVAS_DATA, IS_PROJECT_CHANGED } from '../actionTypes/graph';
 import { REQUEST_OPENSOURCE, RECEIVE_OPENSOURCE } from '../../actionTypes/OpenSource';
-// import { gsdata } from '../../Utils';
 
 // import { createActionWithTypeAndPayload } from '../ActionTemplates';
 
