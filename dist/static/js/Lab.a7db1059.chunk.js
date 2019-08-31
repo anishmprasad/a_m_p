d8a594fd88cf495fc4ddf3e9fb5d6128cae33110
Ghost <ghost@Ghosts-MacBook-Pro.local>
2019-08-31 21:44:31 +0530

    1.0.5

---
+++ dist/static/js/Lab.a7db1059.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{371:function(e,n,t){"use strict";t.r(n),t.d(n,"default",function(){return l});var a=t(0),c=t.n(a);function l(e){return c.a.createElement("div",{className:"lab"},c.a.createElement("h3",null,"welcome to my lab"))}}}]);
+//# sourceMappingURL=Lab.a7db1059.chunk.js.map
--- dist/static/js/Lab.c9a832cc.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{374:function(e,n,t){"use strict";t.r(n),t.d(n,"default",function(){return l});var a=t(1),c=t.n(a);function l(e){return c.a.createElement("div",{className:"lab"},c.a.createElement("h3",null,"welcome to my lab"))}}}]);
-//# sourceMappingURL=Lab.c9a832cc.chunk.js.map
